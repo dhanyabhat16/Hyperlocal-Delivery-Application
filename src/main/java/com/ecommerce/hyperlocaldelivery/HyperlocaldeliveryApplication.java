1b2a988e0b5a0797817453eb7fe619180720e5ff
Dhruthi-Kedilaya <dhruthikedilaya@gmail.com>
2026-04-14 13:16:05 +0530

    Initial commit - Hyperlocal Delivery System

---
+++ src/main/java/com/ecommerce/hyperlocaldelivery/HyperlocaldeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.hyperlocaldelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperlocaldeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperlocaldeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/hyperlocaldelivery/HyperlocaldeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.hyperlocaldelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperlocaldeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
